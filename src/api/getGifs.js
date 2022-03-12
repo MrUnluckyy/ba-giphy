46175da83d16ef20e1a1277ea9a46b775ef015a2
Justas Sobutas <justas@bitdegree.org>
2022-03-12 15:42:07 +0200

    Create gif request function

---
+++ src/api/getGifs.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getRandomGif = async () => {
+    return axios.get('https://api.giphy.com/v1/gifs/random', {
+        params: {
+            api_key: 'BaC7RwDgyCEimoFGdOcEWLHA0uugOLwU',
+        }
+    }).then(resp => {
+        return resp.data.data
+    }).catch(err => {
+        alert(err);
+    })
+}
